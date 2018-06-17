15ca6d4af4eea5d5d1a307c7b483bebc7d1f0ec2
Meghana <meghana@ubuntu.ubuntu-domain>
2018-06-17 19:02:54 +0530

    Add retry to background load

--- bg_job.c
+++ bg_job.c
@@ -79,41 +79,53 @@ double get_wall_time()
  }
 
 
+int schedule_job(float runtime, float period, float deadline){
+	/////////////////////////////////////////////////////
+		struct sched_attr attr;
+		unsigned int flags = 0;
+
+		attr.size = sizeof(attr);
+		attr.sched_flags = 0;
+		attr.sched_nice = 0;
+		attr.sched_priority = 0;
+		
+		attr.sched_policy = SCHED_DEADLINE;
+		attr.sched_runtime = runtime*1000*1000;
+		attr.sched_period = period*1000*1000;
+		attr.sched_deadline=deadline*1000*1000;		
+		return sched_setattr(getpid(), &attr, flags);
+
+
+
+}
+ 
 
 
 int main(int argc, char const *argv[])
 {
 	float period=atof(argv[1]);
 	float runtime=atof(argv[0])*period;
+	float deadline=period;
 	printf("exec successful\n");
 	printf("util: %s\n",argv[0]);
 	printf("period: %s\n",argv[1]);
 	printf("runtime %f\n",runtime);
 
-	int job_id;
+	int jobid;
+	int ret;
 	srand(time(NULL));
-	job_id=rand();
+	jobid=rand();
+	do{
+		usleep(1000000);
+		ret=schedule_job(runtime,period,deadline);
+		if(ret<0){
+			perror("sched_setattr");
+			printf("Error: Job Id: %d\n",jobid );
+		}
 
-	struct sched_attr attr;
-	unsigned int flags = 0;
-	attr.size = sizeof(attr);
-	attr.sched_flags = 0;
-	attr.sched_nice = 0;
-	attr.sched_priority = 0;
-	attr.sched_policy = SCHED_DEADLINE; 
-
-	attr.sched_deadline =period*1000000;
-	attr.sched_period = period*1000000;
-	attr.sched_runtime=runtime*1000000;
-	int ret = sched_setattr(getpid(), &attr, flags);
-	printf("SCHED_DEADLINE RETURN: %d",ret);
-	if (ret < 0) {
-		done = 0;
-		perror("sched_setattr");
-		printf("Error!! Job ID: %d",job_id);
-		exit(-1);
 	}
-
+	while(ret<0);
+	
 
 	float slice = 0.05;
 	double start_wall,end_wall;
@@ -123,7 +135,7 @@ int main(int argc, char const *argv[])
 
 	double cpuTime = cpu_t;
 	double slice_cput = cpuTime/((double)period/slice);
-	double total_sleep_time = (0.95*period)*0.95;
+	double total_sleep_time = period-runtime;
 	double slice_sleep = total_sleep_time / ((double)period/slice);
 	slice_sleep *= 1000000;
 	float total=0;
@@ -161,7 +173,7 @@ int main(int argc, char const *argv[])
 		// sprintf(util,"%f %f\n",(float)num/(time_spent),total);
 		
 	}
-	printf("BG job ending: %d\n",job_id);
+	printf("BG job ending: %d\n",jobid);
 
 	return 0;
 }
